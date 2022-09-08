e0bdb22d526d38ad1b4bd9085c05c3b086c6114d
Kazune Shidara <kazuneshidara@gmail.com>
2022-09-07 22:56:26 -0700

    implementing state for dark and light mode on static page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./Navbar"
+import Main from "./Main"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+
+
+    const [darkMode, setDarkMode] = React.useState(false) 
+
+
+    function toggleDarkMode(){
+
+        setDarkMode(prevMode => !prevMode)
+
+    }
+
+
+
+    return (
+        <div className="container">
+            <Navbar 
+                toggleDarkMode={toggleDarkMode}
+                darkMode={darkMode} 
+            />
+            <Main 
+                darkMode={darkMode} 
+            />
+        </div>
+    )
 }
-
-export default App;
